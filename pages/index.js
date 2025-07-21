fb2da6afcd64a4899ac0a37d6a3994aad224baac
paulin0km <lordkillermano@gmail.com>
2025-07-21 11:04:23 +0000

    Arrumando index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home () {
-    return <h1>Bem-aventurados os que choram, porque serão consolados;<br> bem-aventurados os humildes, porque herdarão a terra;</br> bem-aventurados os mansos, porque herdarão a terra.</h1>;
+    return <h1>Bem-aventurados os que choram, porque serão consolados;
+        <br />bem-aventurados os humildes, porque herdarão a terra;
+        <br />bem-aventurados os mansos, porque herdarão a terra.</h1>;
 }
 
 export default Home;
