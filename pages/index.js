d5fa4f6d6ffbe0858498c43d19821b1782afbe6e
paulin0km <lordkillermano@gmail.com>
2025-07-21 10:39:11 +0000

    Reformula a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Oi pessoass</h1>
+    return <h1>bem aventurado os que choram por que serão consolados, bem aventurado os humilhados, porque eles serão exaltados, bem aventurado os manso de coração por que eles herdarão o reino de Deus.</h1>
 }
 
 export default Home;
