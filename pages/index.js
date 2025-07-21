9be08b6629f31b478ff58e52c7a9701cbd32fa19
paulin0km <lordkillermano@gmail.com>
2025-07-21 11:28:00 +0000

    Reformula a frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home () {
     return <h1>Bem-aventurados os que choram, porque serão consolados;
-        <br />bem-aventurados os humildes, porque herdarão a terra;
-        <br />bem-aventurados os mansos, porque herdarão a terra.</h1>;
+        <br />bem-aventurados os humildes, porque serão exaltados;
+        <br />bem-aventurados os mansos de coração, porque herdarão o reino de Deus.</h1>;
 }
 
 export default Home;
