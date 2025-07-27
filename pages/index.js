53b7f7d21b69365b741c3477361380c5ba558d1e
paulin0km <lordkillermano@gmail.com>
2025-07-27 18:25:04 +0000

    Adiciona uma frase pra `gata`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home () {
-    return <h1>Bem-aventurados os que choram, porque serão consolados;
-        <br />bem-aventurados os humildes, porque serão exaltados;
-        <br />bem-aventurados os mansos de coração, porque herdarão o reino de Deus.</h1>;
+    return <h1>oiii alicyy, nome foda pra krl, pique gringo states unites</h1>;
 }
-
+function teste () {
+  console.log('teste')
+}
 export default Home;
 
