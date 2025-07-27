ed6ea49f432fc7b7883bcbb0486ba189b52054e1
paulin0km <lordkillermano@gmail.com>
2025-07-27 22:35:13 +0000

    Adiciona os scripts `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
-function Home () {
-    return <h1>oiii alicyy, nome foda pra krl, pique gringo states unites</h1>;
+function Home() {
+  return <h1>oiii alicyy, nome foda pra krl, pique gringo states unites</h1>;
 }
-function teste () {
-  console.log('teste')
-}
-export default Home;
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentaçao nada a ve");
+}
+
+export default Home;
