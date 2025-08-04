799b9c30dee1bea9f73952e329022d33ff741604
paulin0km <lordkillermano@gmail.com>
2025-08-04 23:18:57 +0000

    mend

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+//usei a ia pra fazer esse useEffect mas n sei usar nada disso ainda
 import { useEffect } from "react";
-
+//importo oq filho?? from react?? que??
 function Home() {
   useEffect(() => {
     window.confirm('Paulin diz ta dizido');
   }, []);
-
+//
   return (
     <h1>Salve, comprei esse domínio pra fazer meu portifólio?</h1>
   );
